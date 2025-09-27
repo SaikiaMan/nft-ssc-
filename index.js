2974aaf30fb7ab4165f499cba72cdaf39bdb33db
SaikiaMan <saikiamanobraj@gmail.com>
2025-09-27 11:21:47 +0530

    index.js

--- index.js
+++ index.js
@@ -2,122 +2,89 @@ const { readFileSync, writeFileSync, readdirSync, rmSync, existsSync, mkdirSync 
 const sharp = require('sharp');
 
 const template = `
-    <svg width="256" height="256" viewBox="0 0 256 256" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <!-- bg -->
-        <!-- head -->
-        <!-- hair -->
-        <!-- eyes -->
-        <!-- nose -->
-        <!-- mouth -->
-        <!-- beard -->
-    </svg>
-`
+  <svg width="256" height="256" viewBox="0 0 256 256" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <!-- layerA -->
+    <!-- layerB -->
+    <!-- layerC -->
+    <!-- layerD -->
+  </svg>
+`;
 
 const takenNames = {};
-const takenFaces = {};
+const takenCombos = {};
 let idx = 999;
 
-function randInt(max) {
-    return Math.floor(Math.random() * (max + 1));
-}
-
-function randElement(arr) {
-    return arr[Math.floor(Math.random() * arr.length)];
-}
-
+function randInt(max) { return Math.floor(Math.random() * (max + 1)); }
+function randElement(arr) { return arr[Math.floor(Math.random() * arr.length)]; }
 
 function getRandomName() {
-    const adjectives = 'fired trashy tubular brandynpm  jacked swol buff ferocious firey flamin agnostic artificial bloody crazy cringey crusty dirty eccentric glutinous harry juicy simple stylish awesome creepy corny freaky shady sketchy lame sloppy hot intrepid juxtaposed killer ludicrous mangy pastey ragin rusty rockin sinful shameful stupid sterile ugly vascular wild young old zealous flamboyant super sly shifty trippy fried injured depressed anxious clinical'.split(' ');
-    const names = 'aaron bart chad dale earl fred grady harry ivan jeff joe kyle lester steve tanner lucifer todd mitch hunter mike arnold norbert olaf plop quinten randy saul balzac tevin jack ulysses vince will xavier yusuf zack roger raheem rex dustin seth bronson dennis'.split(' ');
-    
-    const randAdj = randElement(adjectives);
-    const randName = randElement(names);
-    const name =  `${randAdj}-${randName}`;
-
-
-    if (takenNames[name] || !name) {
-        return getRandomName();
-    } else {
-        takenNames[name] = name;
-        return name;
-    }
+  const adjectives = 'fired trashy tubular nasty jacked swol buff ferocious firey flamin agnostic artificial bloody crazy cringey crusty dirty eccentric glutinous harry juicy simple stylish awesome creepy corny freaky shady sketchy lame sloppy hot intrepid juxtaposed killer ludicrous mangy pastey ragin rusty rockin sinful shameful stupid sterile ugly vascular wild young old zealous flamboyant super sly shifty trippy fried injured depressed anxious clinical'.split(' ');
+  const names = 'aaron bart chad dale earl fred grady harry ivan jeff joe kyle lester steve tanner lucifer todd mitch hunter mike arnold norbert olaf plop quinten randy saul balzac tevin jack ulysses vince will xavier yusuf zack roger raheem rex dustin seth bronson dennis'.split(' ');
+  const name = `${randElement(adjectives)}-${randElement(names)}`;
+  if (takenNames[name] || !name) return getRandomName();
+  takenNames[name] = name;
+  return name;
 }
 
-function getLayer(name, skip=0.0) {
-    const svg = readFileSync(`./layers/${name}.svg`, 'utf-8');
-    const re = /(?<=\<svg\s*[^>]*>)([\s\S]*?)(?=\<\/svg\>)/g
-    const layer = svg.match(re)[0];
-    return Math.random() > skip ? layer : '';
+function getLayer(name, skip = 0.0) {
+  if (Math.random() < skip) return '';
+  const svg = readFileSync(`./layers/${name}.svg`, 'utf-8');
+  const re = /(?<=\<svg\s*[^>]*>)([\s\S]*?)(?=\<\/svg\>)/g;
+  const match = svg.match(re);
+  return match ? match[0] : '';
 }
 
 async function svgToPng(name) {
-    const src = `./out/${name}.svg`;
-    const dest = `./out/${name}.png`;
-
-    const img = await sharp(src);
-    const resized = await img.resize(1024);
-    await resized.toFile(dest);
+  const src = `./out/${name}.svg`;
+  const dest = `./out/${name}.png`;
+  const img = await sharp(src);
+  const resized = await img.resize(1024);
+  await resized.toFile(dest);
 }
 
+const DIMENSIONS = [
+  { key: 'LayerA', prefix: 'A', maxIndex: 5 },
+  { key: 'LayerB', prefix: 'B', maxIndex: 7 },
+  { key: 'LayerC', prefix: 'C', maxIndex: 4 },
+  { key: 'LayerD', prefix: 'D', maxIndex: 6 },
+];
 
-function createImage(idx) {
-
-    const bg = randInt(5);
-    const hair = randInt(7);
-    const eyes = randInt(9);
-    const nose = randInt(4); 
-    const mouth = randInt(5);
-    const beard = randInt(3);
-    // 18,900 combinations
-
-    const face = [hair, eyes, mouth, nose, beard].join('');
-
-    if (face[takenFaces]) {
-        createImage();
-    } else {
-        const name = getRandomName()
-        console.log(name)
-        face[takenFaces] = face;
-
-        const final = template
-            .replace('<!-- bg -->', getLayer(`bg${bg}`))
-            .replace('<!-- head -->', getLayer('head0'))
-            .replace('<!-- hair -->', getLayer(`hair${hair}`))
-            .replace('<!-- eyes -->', getLayer(`eyes${eyes}`))
-            .replace('<!-- nose -->', getLayer(`nose${nose}`))
-            .replace('<!-- mouth -->', getLayer(`mouth${mouth}`))
-            .replace('<!-- beard -->', getLayer(`beard${beard}`, 0.5))
-
-        const meta = {
-            name,
-            description: `A drawing of ${name.split('-').join(' ')}`,
-            image: `${idx}.png`,
-            attributes: [
-                { 
-                    beard: '',
-                    rarity: 0.5
-                }
-            ]
-        }
-        writeFileSync(`./out/${idx}.json`, JSON.stringify(meta))
-        writeFileSync(`./out/${idx}.svg`, final)
-        svgToPng(idx)
-    }
-
-
+function pickCombo() {
+  return DIMENSIONS.map(dim => {
+    const i = randInt(dim.maxIndex);
+    return { key: dim.key, file: `${dim.prefix}${i}`, idx: i };
+  });
 }
 
-
-
-if (!existsSync('./out')){
-    mkdirSync('./out');
+function comboKey(picks) {
+  return picks.map(p => `${p.key}:${p.idx}`).join('|');
 }
 
+async function createImage(n) {
+  const picks = pickCombo();
+  const key = comboKey(picks);
+  if (takenCombos[key]) return createImage(n);
+  takenCombos[key] = true;
+  const name = getRandomName();
+  let final = template;
+  const placeholders = ['<!-- layerA -->', '<!-- layerB -->', '<!-- layerC -->', '<!-- layerD -->'];
+  picks.forEach((p, i) => {
+    const ph = placeholders[i] || `<!-- ${p.key} -->`;
+    final = final.replace(ph, getLayer(p.file));
+  });
+  const attributes = picks.map(p => ({ trait_type: p.key, value: p.file }));
+  const meta = { name, description: `Generated ${name.split('-').join(' ')}`, image: `${n}.png`, attributes };
+  writeFileSync(`./out/${n}.json`, JSON.stringify(meta));
+  writeFileSync(`./out/${n}.svg`, final);
+  await svgToPng(n);
+}
 
+if (!existsSync('./out')) mkdirSync('./out');
 readdirSync('./out').forEach(f => rmSync(`./out/${f}`));
 
-
-do {
-    createImage(idx);
+(async () => {
+  do {
+    await createImage(idx);
     idx--;
   } while (idx >= 0);
+})();
